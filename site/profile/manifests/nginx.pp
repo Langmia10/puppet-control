b0efaff2c82ca60fea62bb571adf86f9b03f1e95
root <root@testmaster.xvg>
2017-11-28 21:22:47 -0500

    fix issues

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -1,12 +1,8 @@
 class profile::nginx (
- Array $vhosts,
 ) {
    include ::nginx 
  
-   $vhosts.each |$vhost| {
       ::nginx::vhost { $vhost['fqdn']:
-         port           => $vhost['port'],
-         server_aliases => $vhost['server_aliases'],   
-  }
  }
 }
+
